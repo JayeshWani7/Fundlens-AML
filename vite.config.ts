3cd3fe477aa61563cf22cf353b2c358f384c97a4
JayeshWani7 <wanijayesh0@gmail.com>
2026-06-01 21:12:41 +0530

    fix:change port for backend

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
-  const apiProxyTarget = env.VITE_API_PROXY_TARGET || 'http://localhost:8000'
+  const apiProxyTarget = env.VITE_API_PROXY_TARGET || 'http://localhost:8001'
   const wsProxyTarget = env.VITE_WS_PROXY_TARGET || 'ws://localhost:8000'
 
   return {
